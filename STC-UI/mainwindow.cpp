6cf15cca9a1206b163ab75353dc644dd198cf4ca
Abraham Tsang <abraham.tsang@gmail.com>
2020-11-10 22:52:18 -0500

    converted input to list of strings

--- STC-UI/mainwindow.cpp
+++ STC-UI/mainwindow.cpp
@@ -1,6 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+
+#include <iostream>
 #include <string>
+#include <vector>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -17,6 +20,21 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_enterButton_clicked()
 {
-    QString str = ui->listInput->toPlainText();
-    ui->label->setText(str);
+    QString qstr = ui->listInput->toPlainText();
+    std::string str = qstr.toStdString();
+    ui->label->setText(qstr);
+    std::vector<std::string> strs;
+    std::string temp;
+    strs.push_back(temp);
+    for(int i = 0; i < str.size(); i++){
+        if(str[i] == ' '){
+            strs.push_back(temp);
+        }
+        else{
+            strs[strs.size()-1].push_back(str[i]);
+        }
+    }
+    for(int i = 0; i < strs.size(); i++){
+        std::cout << strs[i] << std::endl;
+    }
 }
--- STC-UI/mainwindow.h
+++ STC-UI/mainwindow.h
@@ -2,7 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <vector>
 namespace Ui {
 class MainWindow;
 }
@@ -20,6 +20,7 @@ private slots:
 
 private:
     Ui::MainWindow *ui;
+    std::vector<int> list;
 };
 
 #endif // MAINWINDOW_H
