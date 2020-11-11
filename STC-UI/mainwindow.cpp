1e7330190a8755a677ee02b05b3b59b50e13c3f6
Abraham Tsang <abraham.tsang@gmail.com>
2020-11-10 23:01:13 -0500

    converted list of strings to list of ints

--- STC-UI/mainwindow.cpp
+++ STC-UI/mainwindow.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <sstream>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -24,17 +25,21 @@ void MainWindow::on_enterButton_clicked()
     std::string str = qstr.toStdString();
     ui->label->setText(qstr);
     std::vector<std::string> strs;
-    std::string temp;
-    strs.push_back(temp);
+    std::string empty = "";
+    strs.push_back(empty);
     for(int i = 0; i < str.size(); i++){
         if(str[i] == ' '){
-            strs.push_back(temp);
+            strs.push_back(empty);
         }
         else{
             strs[strs.size()-1].push_back(str[i]);
         }
     }
+    int temp = 0;
     for(int i = 0; i < strs.size(); i++){
-        std::cout << strs[i] << std::endl;
+        std::stringstream ss(strs[i]);
+        ss >> temp;
+        list.push_back(temp);
+        //std::cout << list[i] << std::endl;
     }
 }
